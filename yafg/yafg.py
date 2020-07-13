5bbea662dbc83e40027ed85c550d00556dd3764e
Philipp Trommler <yafg@philipp-trommler.me>
2020-07-13 20:48:46 +0200

    Update copyright years

--- yafg/yafg.py
+++ yafg/yafg.py
@@ -1,6 +1,6 @@
 # yafg: Yet Another Figure Generator
 #
-# Copyright (c) 2019 Philipp Trommler
+# Copyright (c) 2019-2020 Philipp Trommler
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 from markdown.extensions import Extension
